58e3336ec1903f104f0dc260297a5546e9dfe4e1
camellia2001 <126091895+camellia2001@users.noreply.github.com>
2024-07-06 10:24:40 +0000

    config commit done

---
+++ basic/date.js
@@ -0,0 +1,15 @@
+
+
+//let mydate=new Date();
+// console.log(mydate.toDateString());
+// console.log(typeof(mydate));
+ 
+//  let somoy = Date.now();
+//  console.log(somoy/1000);
+
+// let mydate=new Date();
+// if(mydate.getDay()==6)
+//     console.log("FRIDAY");
+// console.log(mydate.getDate()+"/"+mydate.getMonth()+"/"+mydate.getUTCFullYear());
+// console.log(mydate.getHours()+" Hours : "+mydate.getMinutes()+" Minutes : "+mydate.getUTCSeconds()+" Second ");
+
