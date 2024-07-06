964acd8044019f53782348627764dc779c8e3770
camellia2001 <126091895+camellia2001@users.noreply.github.com>
2024-07-06 06:21:01 +0000

    config commit done

---
+++ basic/stm.js
@@ -0,0 +1,19 @@
+// const s=new String("camellia");
+// console.log(s.toUpperCase());
+// console.log(s.charAt(5));
+
+// // string method------
+
+// console.log(s.indexOf('m'));
+
+// console.log(s.substring(1,4));
+
+// console.log(s.slice(-4,-1));
+
+// var st=" camie  ";
+// console.log(st.trim());
+// console.log(st.replace('i','u'));
+
+
+// var s="hellow world";
+// console.log(s.split(' '));
