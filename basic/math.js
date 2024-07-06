712ddf94debe3cb837a74940d15dea1c20cf389f
camellia2001 <126091895+camellia2001@users.noreply.github.com>
2024-07-06 08:56:06 +0000

    config commit done

---
+++ basic/math.js
@@ -0,0 +1,19 @@
+// const num=123;
+// console.log(num);
+
+// const n= new Number(123);
+// console.log(n.toString());
+// console.log(typeof(n));
+ 
+// console.log(n.toFixed(2)); //fix decimal point
+ 
+// const f=135.657840;
+// console.log(f.toLocaleString(2));
+
+console.log(Math);
+console.log(Math.abs(-4));
+console.log(Math.round(34.2));
+console.log(Math.ceil(34.2)); 
+console.log(Math.floor(34.2));
+console.log(Math.max(2,4,5));
+console.log(Math.min(2,4,5));
