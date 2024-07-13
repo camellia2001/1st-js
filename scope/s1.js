587658a9b2c87716cfca2cef334e07ba80b19ed8
camellia2001 <126091895+camellia2001@users.noreply.github.com>
2024-07-13 15:26:18 +0000

    config commit done

--- scope/s1.js
+++ scope/s1.js
@@ -1,6 +1,17 @@
-var a=28;
-if(true)
-{
-    let a=2;
-}
-console.log(a);
+// var a=28;
+// if(true)
+// {
+//     let a=2;
+// }
+// console.log(a);
+
+// function fun1()
+// {
+//   console.log("hello user1")
+// }
+// fun1();
+
+// (function fun2(name="muna")
+// {
+//     console.log(`hello user2 ${name}`)
+// })("camellia");
