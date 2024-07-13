d9e8ea7ff44a463fcedd13347172a24fdaed79dd
camellia2001 <126091895+camellia2001@users.noreply.github.com>
2024-07-13 19:23:32 +0000

    config commit done

--- loop/l1.js
+++ loop/l1.js
@@ -1,10 +1,41 @@
 //for loop
-for(let i=1;i<=10;i++)
-{
-  console.log(`table of ${i}`);
-  console.log(`------------------`);
-  for(let j=1;j<=10;j++)
-  {
-    console.log(i+'*'+j+'='+i*j);
-  }
-}
+// for(let i=1;i<=10;i++)
+// {
+//   console.log(`table of ${i}`);
+//   console.log(`------------------`);
+//   for(let j=1;j<=10;j++)
+//   {
+//     console.log(i+'*'+j+'='+i*j);
+//   }
+// }
+
+// const a=[1,2,3,4,5];
+// for(let i=0;i<a.length;i++)
+// {
+//     if(a[i]%2===0)
+//         console.log(a[i]);
+// }
+
+//while loop
+// let i=2,c;
+// while(i<=20)
+// {
+//     c=0;
+//     for(let j=2;j<i;j++)
+//     {
+//         if(i%j==0)
+//             c++;
+//         if(c>0)
+//             break;
+//     }
+//     if(c==0)
+//         console.log(i);
+//     i++;
+// }
+
+//do while
+// let a=11;
+// do{
+//     console.log(a);
+//     a++;
+// }while(a<10);
